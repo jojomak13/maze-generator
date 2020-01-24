c6dbf2a5e588fe98799797093499312ab4fc796b
jojomak13 <jojomak350@gmail.com>
2020-01-24 19:40:30 +0200

    create maze generator

--- js/app.js
+++ js/app.js
@@ -2,24 +2,52 @@ let base = {
         cols: 0,
         rows: 0
     },
-    grid = [];
+    grid = [],
+    stack = [],
+    current;
 
-const w = 50;
+const w = 20;
 
 function setup() {
-    createCanvas(450, 450);
+    createCanvas(440, 440);
+
+    frameRate(50);
 
     base.cols = floor(width / w);
     base.rows = floor(height / w);
 
     initCells();
+
+    current = grid[0];
 }
 
 function draw() {
-    background(52);
+    background(50);
 
     for (let i = 0, len = grid.length; i < len; i++) {
         grid[i].draw(w);
+    }
+
+    current.visited = true;
+    current.highLight();
+
+    // Step1: Check if the current cell has neighbours
+    let nextCell = current.getRandomNeighbour();
+    if (nextCell) {
+        nextCell.visited = true;
+
+        // Step 2: push the current cell into the stack
+        stack.push(current);
+
+        // Step 3: Remove Walls between the current and next cell
+        removeWall(current, nextCell);
+
+        // Step 4: Set the next cell as current cell
+        current = nextCell;
+
+        // Step 5: if the stack not empty
+    } else if (stack.length != 0) {
+        current = stack.pop();
     }
 }
 
@@ -33,5 +61,37 @@ function initCells() {
         for (let col = 0; col < base.cols; col++) {
             grid.push(new Cell(row, col));
         }
+    }
+}
+
+/**
+ *
+ * @param {Cell} current
+ * @param {Cell} next
+ */
+function removeWall(current, next) {
+    let x = current.x - next.x;
+    let y = current.y - next.y;
+
+    // move right
+    if (x === -1) {
+        current.walls[1] = false;
+        next.walls[3] = false;
+
+        // Move Left
+    } else if (x === 1) {
+        current.walls[3] = false;
+        next.walls[1] = false;
+    }
+
+    // Move Top
+    if (y === 1) {
+        current.walls[0] = false;
+        next.walls[2] = false;
+
+        // Move Bottom
+    } else if (y === -1) {
+        current.walls[2] = false;
+        next.walls[0] = false;
     }
 }
--- js/cell.js
+++ js/cell.js
@@ -7,6 +7,8 @@ class Cell {
     constructor(x, y) {
         this.x = x;
         this.y = y;
+        this.visited = false;
+        this.walls = [true, true, true, true];
     }
 
     /**
@@ -19,12 +21,66 @@ class Cell {
 
         stroke(255);
 
-        line(x, y, x + w, y);
-        line(x, y, x, y + w);
+        // top Wall
+        if (this.walls[0]) line(x, y, x + w, y);
 
-        line(x + w, y + w, x + w, y);
-        line(x + w, y + w, x, y + w);
-        // noFill();
-        // rect(x, y, w, w);
+        // Right Wall
+        if (this.walls[1]) line(x + w, y + w, x + w, y);
+
+        // Bottom Wall
+        if (this.walls[2]) line(x + w, y + w, x, y + w);
+
+        // Left Wall
+        if (this.walls[3]) line(x, y, x, y + w);
+
+        if (this.visited) {
+            noStroke();
+            fill("#8BC34A");
+            rect(x, y, w, w);
+        }
+    }
+
+    /**
+     *
+     * @param {*} x
+     * @param {*} y
+     */
+    position(x, y) {
+        if (x < 0 || y < 0 || x > base.rows - 1 || y > base.cols - 1) {
+            return -1;
+        }
+
+        return y + x * base.cols;
+    }
+
+    /**
+     * Get a random neighbour that is availabe neighbor
+     * for the current cell
+     */
+    getRandomNeighbour() {
+        let neighbours = [];
+
+        let top = grid[this.position(this.x, this.y - 1)];
+        let bottom = grid[this.position(this.x, this.y + 1)];
+        let left = grid[this.position(this.x - 1, this.y)];
+        let right = grid[this.position(this.x + 1, this.y)];
+
+        if (top && !top.visited) neighbours.push(top);
+        if (right && !right.visited) neighbours.push(right);
+        if (bottom && !bottom.visited) neighbours.push(bottom);
+        if (left && !left.visited) neighbours.push(left);
+
+        if (neighbours.length === 0) return undefined;
+
+        return neighbours[floor(random(0, neighbours.length))];
+    }
+
+    highLight() {
+        let x = this.x * width;
+        let y = this.y * width;
+
+        noStroke();
+        fill("#090");
+        rect(x, y, w, w);
     }
 }
